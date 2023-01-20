879b079dc1b6b9762a64b8b56850f5ec0c166768
Marcelo Both <marcelo.both@gmail.com>
2023-01-20 20:30:20 -0300

    lint: fixes rubocop issues

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 require 'simplecov'
 SimpleCov.start do
-  add_filter "/config/"
-  add_filter "/spec/"
+  add_filter '/config/'
+  add_filter '/spec/'
 end
 
 # Previous content of test helper now starts here
