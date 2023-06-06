d4888cf0c987e772c2c871c9e1d00a7bb72468da
Marcelo Both <marcelo.both@gmail.com>
2023-06-06 09:21:39 -0300

    test: fixes specs to use factories on user creation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,10 +43,10 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Devise helpers
-  config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
