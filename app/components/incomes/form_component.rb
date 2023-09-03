606b15bc51f9cdef6cd8e30dd594e8e752b19054
Marcelo Both <marcelo.both@gmail.com>
2023-09-03 09:01:39 -0300

    feat: adds a way to display validation information

--- app/components/incomes/form_component.rb
+++ app/components/incomes/form_component.rb
@@ -2,5 +2,25 @@
 
 module Incomes
   class FormComponent < BaseViewComponent
+    attr_accessor :income, :description, :value, :received_at
+
+    def initialize(income: nil)
+      @income = income
+      income_attributes
+    end
+
+    def income_attributes
+      @description = income&.description
+      @value = income&.value
+      @received_at = income&.received_at
+    end
+
+    def submit_url
+      income ? income_path(income) : incomes_path
+    end
+
+    def submit_method
+      income ? :patch : :post
+    end
   end
 end
